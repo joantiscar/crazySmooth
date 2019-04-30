aa8bab9f17e2905ae73f620b9cae3980d62dffe6
joantiscar <joantiscar@gmail.com>
2019-04-30 18:38:04 +0200

    Sync

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,61 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <div id="game"></div>
 </template>
+<script>
+  import Phaser from 'phaser'
+  import tileSet from './assets/tileset.png'
+  import itemSet from './assets/tileset.png'
+  import firstmap_Tiles from './assets/maps/firstmap_Tiles.csv'
+  import firstmap_Items from './assets/maps/firstmap_Items.csv'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  export default {
+    name: 'Game',
+    mounted () {
+      // PHASER 2.6 -> phaser-ce
+      // Phaser 3.0 -> phaser
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+      let config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+          default: 'arcade',
+          arcade: {
+            gravity: {
+              y: 100
+            }
+          }
+        },
+        // NO HI HA STATES A 3.0 -> SCENES
+        scene: {
+          preload() {
+            console.log('PRELOAD')
+            this.load.image('tileset', tileset);
+            this.load.tilemapCSV('firstmap_Items', firstmap_Items)
+            this.load.tilemapCSV('firstmap_Tiles', firstmap_Tiles)
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+          },
+          create() {
+            let map = this.make.tilemap({key: "map", tileWidth: 16, tileHeight: 16});
+            let tileSet = map.addTilesetImage("tiles");
+            let itemSet = map.addTilesetImage("items");
+            let tilesLayer = map.createStaticLayer(0, tileSet, 0, 0); // layer index, tileset, x, y
+            let itemsLayer = map.createStaticLayer(0, itemSet, 0, 0); // layer index, tileset, x, y
+            console.log('CREATE')
+            // INITIALIZE del nivell -> Afegirem tiles (level: pareds, terres), afegirem players, collectibles (coins)
+            this.cameras.main.backgroundColor.setTo(69, 911, 420)
+
+            this.cursors = this.input.keyboard.createCursorKeys()
+
+          },
+          update() {
+          }
+        }
+      }
+      // eslint-disable-next-line
+      new Phaser.Game(config)
+    },
+    methods: {}
+  }
+</script>
+<style scoped></style>
---
+++ src/assets/maps/Items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Items" tilewidth="8" tileheight="8" tilecount="144" columns="12">
+ <image source="../items/pico 8 Item tiles 01.png" width="96" height="96"/>
+</tileset>
