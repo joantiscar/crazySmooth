f6d84909bbcc5522a1bcc48b8197f46c3cd3e77c
joantiscar <joantiscar@gmail.com>
2019-05-13 20:30:35 +0200

    Sync

--- src/assets/maps/ileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Tileset" tilewidth="8" tileheight="8" tilecount="144" columns="12">
- <image source="../items/pico 8 tiles.png" width="96" height="96"/>
-</tileset>
