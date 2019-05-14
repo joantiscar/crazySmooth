fbf9e5b4ca963dbc2739b9b1ab836fa35b098cef
joantiscar <joantiscar@gmail.com>
2019-05-14 21:16:38 +0200

    Sync

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,16 @@
 <template>
-  <div id="game"></div>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
-<script>
-  import Phaser from 'phaser'
-  import firstmap_Tiles from './assets/maps/firstmap_Tiles.csv'
-  import firstmap_Items from './assets/maps/firstmap_Items.csv'
 
-  export default {
-    name: 'Game',
-    mounted () {
-      // PHASER 2.6 -> phaser-ce
-      // Phaser 3.0 -> phaser
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-      let config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: {
-              y: 100
-            }
-          }
-        },
-        // NO HI HA STATES A 3.0 -> SCENES
-        scene: {
-          preload() {
-            console.log('PRELOAD')
-            this.load.tilemapCSV('firstmap_Items', firstmap_Items)
-            this.load.tilemapCSV('firstmap_Tiles', firstmap_Tiles)
-
-          },
-          create() {
-            let map = this.make.tilemap({key: "map", tileWidth: 16, tileHeight: 16});
-            let tileSet = map.addTilesetImage("tiles");
-            let itemSet = map.addTilesetImage("items");
-            let tilesLayer = map.createStaticLayer(0, tileSet, 0, 0); // layer index, tileset, x, y
-            let itemsLayer = map.createStaticLayer(0, itemSet, 0, 0); // layer index, tileset, x, y
-            console.log('CREATE')
-            // INITIALIZE del nivell -> Afegirem tiles (level: pareds, terres), afegirem players, collectibles (coins)
-            this.cameras.main.backgroundColor.setTo(69, 911, 420)
-
-            this.cursors = this.input.keyboard.createCursorKeys()
-
-          },
-          update() {
-          }
-        }
-      }
-      // eslint-disable-next-line
-      new Phaser.Game(config)
-    },
-    methods: {}
-  }
-</script>
-<style scoped></style>
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,112 @@
+<template>
+  <div id="game"></div>
+</template>
+  <script>
+    /* eslint-disable */
+    import Phaser from 'phaser'
+    import _TILESET_SET from '../assets/tileSets/tileset.png'
+    import _JSON_MAP from '../assets/tileSets/map.json'
+    import _PLAYER from '../assets/tileSets/player.png'
+    var player;
+
+
+    export default {
+      name: 'Game',
+      created() {
+        var controls = false
+        // PHASER 2.6 -> phaser-ce
+        // PHASER 3.0 -> phaser
+        let config = {
+          type: Phaser.AUTO,
+          width: window.innerWidth-25,
+          height: window.innerHeight-20,
+          physics: {
+            default: 'arcade',
+            arcade: {
+              gravity: {
+                y: 100
+              },
+              debug :true
+            }
+          },
+          // NO HI HA STATES A 3.0 -> SCENES
+          scene: {
+            preload() {
+              console.log("PRELOAD");
+              this.load.image('tileset', _TILESET_SET)
+              this.load.tilemapTiledJSON("map",_JSON_MAP)
+              this.load.spritesheet('player',_PLAYER ,{ frameWidth: 28, frameHeight: 22 })
+
+            },
+            create() {
+              const camera = this.cameras.main;
+              console.log("CREATED");
+              let map = this.make.tilemap({ key: "map" })
+              let tileset = map.addTilesetImage("tileset", "tileset")
+              let floor = map.createStaticLayer("floor", tileset, 0, 0)
+              let elevation = map.createStaticLayer("elevation",tileset,0,0)
+              floor.setCollisionByExclusion([-1]);
+              elevation.setCollisionByExclusion([-1]);
+
+
+              // Habilitem un cursor per a les fletxes del teclat
+              this.cursors = this.input.keyboard.createCursorKeys();
+
+              // La càmara no sortirà del món
+              camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+              let spawnpoint = map.findObject('player', obj => obj.name === 'spanwPoint');
+
+              player = this.physics.add.sprite(spawnpoint.x, spawnpoint.y, 'player');
+              this.player.setCollideWorldBounds(true)
+              player.setScale(2);
+              player.setBounce(0.2);
+              player.lives = 3;
+              //
+              // // Animació de correr del player
+              //
+              this.physics.add.collider(player,floor)
+              //
+              this.anims.create({
+                key: 'idle',
+                frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
+                frameRate: 5,
+                repeat: -1
+              })
+              camera.startFollow(player)
+
+            },
+            update () {
+              player.anims.play('idle', true)
+
+              // ESTE EL QUE S'executa continuament al Game loop -> 60 vegades per segon o FPS
+
+              // INPUT EVENTS
+              if (this.cursors.left.isDown) {
+                player.setVelocityX(-160)
+                player.setFrame(2)
+              } else if (this.cursors.right.isDown) {
+                player.setVelocityX(160)
+                player.setFrame(1)
+              } else {
+                if (player.body.velocity.x > 0) player.setVelocityX(player.body.velocity.x - 1)
+                if (player.body.velocity.x < 0) player.setVelocityX(player.body.velocity.x + 1)
+
+                player.setFrame(0)
+              }
+              if (player.body.touching.down && this.cursors.up.isDown) {
+                player.setVelocityY(-200)
+              }
+
+              if (player.body.touching.down && player.y > 10) {
+                // this.dustSound.play()
+              }
+            }
+          }
+        }
+        // eslint-disable-next-line
+        new Phaser.Game(config)
+      }
+    }
+  </script>
+
