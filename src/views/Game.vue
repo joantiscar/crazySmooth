101a7b345ae0340756ffe11a17efcb5882022801
joantiscar <joantiscar@gmail.com>
2019-05-30 20:35:27 +0200

    Dyncc

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,6 +7,7 @@
   import tileset from '../assets/tileSets/tileset.png'
   import enemies_tileset from '../assets/tileSets/enemies.png'
   import map_json from '../assets/tileSets/map.json'
+  import map2_json from '../assets/tileSets/map2.json'
   import jugador from '../assets/tileSets/player.png'
   import start from '../assets/start.png'
   import playMusic from '../assets/playMusic.png'
@@ -169,6 +170,7 @@
       this.load.spritesheet('tileset', tileset, {frameWidth: 16, frameHeight: 16})
       this.load.spritesheet('enemies_tileset', enemies_tileset, {frameWidth: 16, frameHeight: 16})
       this.load.tilemapTiledJSON("map", map_json)
+      this.load.tilemapTiledJSON("map2", map2_json)
       this.load.spritesheet('player', jugador, {frameWidth: 50, frameHeight: 37})
       this.load.image('start', start)
       this.load.image('muteMusic', muteMusic)
@@ -382,6 +384,7 @@
       let elevation = this.map.createStaticLayer("elevation", tileset, 0, 0)
       let death = this.map.createStaticLayer("death", '', 0, 0)
       let warp = this.map.createStaticLayer("warp", tileset, 0, 0)
+      let decoracio = this.map.createStaticLayer("decoracio", tileset, 0, 0)
 
       floor.setCollisionByExclusion([-1]);
       death.setCollisionByExclusion([-1]);
@@ -631,31 +634,59 @@
     constructor() {
       super({key: 'Level2'})
     }
-
-    preload() {
-      console.log("PRELOAD");
-      this.load.spritesheet('tileset', tileset, {frameWidth: 16, frameHeight: 16})
-      this.load.tilemapTiledJSON("map", map_json)
-      this.load.spritesheet('player', jugador, {frameWidth: 50, frameHeight: 37})
-
-    }
-
     create() {
       const camera = this.cameras.main;
-      console.log("CREATED");
-      let map = this.make.tilemap({key: "map"})
+      if (this.sys.game.device.os.desktop) {
+        this.cameras.main.setZoom(2)
+      }
+      let gameBackground = this.add.image(0, 0, 'gameBackground').setOrigin(0).setDepth(0)
+      gameBackground.displayHeight = this.game.renderer.height
+      gameBackground.displayWidth = this.game.renderer.width
+      gameBackground.setScrollFactor(0)
+      this.map = this.make.tilemap({key: "map2"})
       let tileset = this.map.addTilesetImage("tileset", "tileset")
+      let enemies_tileset = this.map.addTilesetImage("enemies_tileset", "enemies_tileset")
       let floor = this.map.createStaticLayer("floor", tileset, 0, 0)
+      let invisiblewalls = this.map.createStaticLayer("invisiblewalls", '', 0, 0)
       let elevation = this.map.createStaticLayer("elevation", tileset, 0, 0)
       let death = this.map.createStaticLayer("death", '', 0, 0)
       let warp = this.map.createStaticLayer("warp", tileset, 0, 0)
+      let decoracio = this.map.createStaticLayer("decoracio", tileset, 0, 0)
 
       floor.setCollisionByExclusion([-1]);
       death.setCollisionByExclusion([-1]);
       warp.setCollisionByExclusion([-1]);
       elevation.setCollisionByExclusion([-1]);
+      invisiblewalls.setCollisionByExclusion([-1]);
       coinLayer = this.map.getObjectLayer('items')['objects']
-      enemyLayer = this.map.getObjectLayer('enemies')['objects']
+
+      var particlesNeu = this.add.particles('neu')
+      particlesNeu.createEmitter(
+        {
+          "active": true,
+          "visible": true,
+          "collideBottom": true,
+          "collideLeft": true,
+          "collideRight": true,
+          "collideTop": true,
+          "on": true,
+          "particleBringToTop": true,
+          "radial": true,
+          "frame": {"frames": ["star_05"], "cycle": false, "quantity": 1},
+          "gravityY": 1200,
+          "timeScale": 1,
+          "alpha": 1,
+          "angle": {"min": 0, "max": 360, "ease": "Linear"},
+          "lifespan": 1000,
+          "maxVelocityX": 10000,
+          "maxVelocityY": 10000,
+          "quantity": 1,
+          "scale": 1,
+          "x": 2,
+          "y": 6,
+          "tint": [16777215],
+          "emitZone": {"source": new Phaser.Geom.Rectangle(0, 0, 3200, 50), "type": "random"}
+        })
 
 
       // Habilitem un cursor per a les fletxes del teclat
@@ -667,18 +698,15 @@
       // La càmara no sortirà del món
       camera.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
 
-      let spawnpoint = this.map.findObject('player', obj => obj.name === 'spanwPoint');
+      this.spawnPoint = this.map.findObject('player', obj => obj.name === 'spanwPoint');
 
 
-      player = this.physics.add.sprite(spawnpoint.x, spawnpoint.y, 'player');
-      player.lives = 3;
+      player = this.physics.add.sprite(this.spawnPoint.x, this.spawnPoint.y, 'player');
+      player.spawnPoint = this.spawnPoint
       //
       // // Animació de correr del player
       //
-      this.physics.add.collider(player, floor, function () {
-        console.log('ESTIC AN TERRA')
-
-      })
+      this.physics.add.collider(player, floor)
       this.physics.add.collider(player, elevation)
       this.physics.add.collider(player, warp, nextLevel, null, this)
       this.physics.add.collider(player, death, takeDamage, null, this)
@@ -715,54 +743,154 @@
       coins = this.physics.add.staticGroup()
       enemies = this.physics.add.group()
       createCoins()
-      createEnemies()
+
+      this.enemy1 = this.physics.add.sprite(580, 281, 'enemies_tileset',280)
+      this.enemy2 = this.physics.add.sprite(770, 281, 'enemies_tileset',280)
+      this.enemy3 = this.physics.add.sprite(957, 281, 'enemies_tileset',280)
+      this.enemy4 = this.physics.add.sprite(1100, 281, 'enemies_tileset',280)
+      this.enemy5 = this.physics.add.sprite(1500, 281, 'enemies_tileset',280)
+      this.enemy6 = this.physics.add.sprite(1700, 281, 'enemies_tileset',280)
+      this.enemy7 = this.physics.add.sprite(1000, 281, 'enemies_tileset',280)
+      this.anims.create({
+        key: 'enemyMove',
+        frames: this.anims.generateFrameNumbers('enemies_tileset', {start: 280, end: 281}),
+        frameRate: 6,
+        repeat: -1
+      })
+
+      this.enemy1.body.velocity.x = 100
+      this.enemy2.body.velocity.x = 143
+      this.enemy3.body.velocity.x = 123
+      this.enemy4.body.velocity.x = 67
+      this.enemy5.body.velocity.x = 44
+      this.enemy6.body.velocity.x = 234
+      this.enemy7.body.velocity.x = 14
+      for (let i = 1; i < 8; i++) {
+        this.physics.add.collider(this['enemy' + i], floor)
+        this.physics.add.collider(this['enemy' + i], elevation)
+        this.physics.add.collider(this['enemy' + i], invisiblewalls)
+        this.physics.add.collider(player, this['enemy' + i], takeDamage, null, this)
+        this['enemy' + i].anims.play('enemyMove')
+      }
+
       death.setCollisionByProperty({collides: true})
 
       this.physics.add.overlap(player, coins, takeCoin, null, this)
       this.physics.add.overlap(player, enemies, takeDamage, null, this)
 
+      if (!this.sys.game.device.os.desktop) {
+        this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+          x: this.cameras.main.centerX - 250,
+          y: this.cameras.main.centerY + 100,
+          radius: 40,
+          base: this.add.graphics().fillStyle(0x888888).fillCircle(0, 0, 50),
+          thumb: this.add.graphics().fillStyle(0xcccccc).fillCircle(0, 0, 30)
+        })
+        this.cursorKeysVirtual = this.joyStick.createCursorKeys()
+
+      }
+      if (!this.sys.game.device.os.desktop) {
+        this.scoreText = this.add.text(100, 10, 'Puntuació: ' + player.score, {
+          fontSize: '16px',
+          fill: '#fff'
+        }).setScrollFactor(0).setDepth(200)
+        this.livesText = this.add.text(10, 10, 'Vides: ' + player.lives, {
+          fontSize: '16px',
+          fill: '#fff'
+        }).setScrollFactor(0).setDepth(200)
+      } else {
+        this.scoreText = this.add.text((window.innerWidth / 3) - 100, (window.innerHeight / 4), 'Puntuació: ' + player.score, {
+          fontSize: '16px',
+          fill: '#fff'
+        }).setScrollFactor(0).setDepth(200)
+        this.livesText = this.add.text((window.innerWidth / 3) + 50, (window.innerHeight / 4), 'Vides: ' + player.lives, {
+          fontSize: '16px',
+          fill: '#fff'
+        }).setScrollFactor(0).setDepth(200)
+      }
+
     }
 
     update() {
+      this.input.update()
+      this.scoreText.setText('Puntuació: ' + player.score)
+      this.livesText.setText('Vides: ' + player.lives)
+
       if (player.body.velocity.x === 0 && !player.isJumping) player.anims.play('idle', true)
 
 
       if (player.isJumping && player.isJumpingAnimation) {
         player.anims.play('jump', false)
+        this.sound.play('jumpSound')
+
         player.isJumpingAnimation = false
 
 
       }
 
       // INPUT EVENTS
-      if (this.cursors.left.isDown) {
-        player.setVelocityX(-160)
-        if (!player.isJumping) player.anims.play('runLeft', true)
-        player.flipX = true
+
+      if (this.sys.game.device.os.desktop) {
+
+        if (this.cursors.left.isDown) {
+          player.setVelocityX(-160)
+          if (!player.isJumping) player.anims.play('runLeft', true)
+          player.flipX = true
 
 
-      } else if (this.cursors.right.isDown) {
-        player.setVelocityX(160)
-        if (!player.isJumping) player.anims.play('runRight', true)
-        player.flipX = false
+        } else if (this.cursors.right.isDown) {
+          player.setVelocityX(160)
+          if (!player.isJumping) player.anims.play('runRight', true)
+          player.flipX = false
 
 
+        } else {
+          if (player.body.velocity.x > 0) player.setVelocityX(player.body.velocity.x - 20)
+          if (player.body.velocity.x < 0) player.setVelocityX(player.body.velocity.x + 20)
+          if (player.body.velocity.x < 20 && player.body.velocity.x > -20) player.setVelocityX(0)
+
+        }
+        if (this.cursors.up.isDown && player.body.onFloor()) {
+          player.setVelocityY(-340) // 340
+          player.isJumping = true
+          player.isJumpingAnimation = true
+
+        }
       } else {
-        if (player.body.velocity.x > 0) player.setVelocityX(player.body.velocity.x - 20)
-        if (player.body.velocity.x < 0) player.setVelocityX(player.body.velocity.x + 20)
-        if (player.body.velocity.x < 20 && player.body.velocity.x > -20) player.setVelocityX(0)
+        if (this.cursorKeysVirtual.left.isDown) {
+          player.setVelocityX(-160)
+          if (!player.isJumping) player.anims.play('runLeft', true)
+          player.flipX = true
 
-      }
-      if (this.cursors.up.isDown && player.body.onFloor()) {
-        player.setVelocityY(-340) // 340
-        player.isJumping = true
-        player.isJumpingAnimation = true
 
+        } else if (this.cursorKeysVirtual.right.isDown) {
+          player.setVelocityX(160)
+          if (!player.isJumping) player.anims.play('runRight', true)
+          player.flipX = false
+
+
+        } else {
+          if (player.body.velocity.x > 0) player.setVelocityX(player.body.velocity.x - 20)
+          if (player.body.velocity.x < 0) player.setVelocityX(player.body.velocity.x + 20)
+          if (player.body.velocity.x < 20 && player.body.velocity.x > -20) player.setVelocityX(0)
+
+        }
+        if (this.cursorKeysVirtual.up.isDown && player.body.onFloor()) {
+          player.setVelocityY(-340) // 340
+          player.isJumping = true
+          player.isJumpingAnimation = true
+
+        }
       }
 
       if (player.body.onFloor() && player.body.velocity.y > -300) {
+        if (player.isJumping) this.sound.play('dustSound')
         player.isJumping = false
+
+
       }
+
+      moveEnemies.call(this)
     }
   }
 </script>
